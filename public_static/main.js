724a0062624ca4339ef259df9a58ccd0d1ba5508
dheerain-jain <jain.dheerain@gmail.com>
2017-07-15 16:08:38 +0530

    bugs removed ; main.js logic changed

--- public_static/main.js
+++ public_static/main.js
@@ -9,20 +9,35 @@
 /*what happens here si that ewhen we insatll the cocket module the file is hosted at a ceratain port
 * this port is local host and this file is present at the server*/
 
-var username=prompt("Please enter  your name ");
+/*var username=prompt("Please enter  your name ");
+*/
+var username = prompt("Enter username");
 
-$(function () {
-    var socket=io();
-    $('#btn').click(function () {
+$(function() {
+    var socket = io();
 
-        var inp =$('#inp').val();
-        socket.emit('rec_message',{name : username , input:inp});
+    $('#btn').click(function(){
+        var inp = $('#inp').val();
+        socket.emit('rec_message',{user: username,input :inp});
+
+
+
+    });
+    socket.on('get',function(data){
+
+        var msg = '<li>' + data.user + ":" + data.input + '</li>';
+        $('#message').append(msg);
 
     });
 
-    socket.on('get',function (data) {
-        var msg= '<li>' +data.name+'  '+data.input+' </li>'
+    socket.on('all',function(data) {
+        data.forEach(function(client){
+            var msg = '<li>' + client.user + ":" +
+                client.input + '</li>';
         $('#message').append(msg);
-        console.log(data);
+    });
+
+
     })
+
 });
--- server.js
+++ server.js
@@ -10,27 +10,40 @@ var server=http.createServer(app);
 
 var socket=require('socket.io');
 
-var io=socket(server); // when ever i make this var /socket.io/socket.io.js this file is hosted on its own at this path
-
+var io = socket(server); // when ever i make this var /socket.io/socket.io.js this file is hosted on its own at this path
 app.use('/',express.static('public_static'));
 
+var a=[];
+/*
 io.on('connection',function (socket) {//every thing is now defined on the argument of the call after the connection
    console.log("Connection established by the io socket");
-   socket.on('disconnect',function () {
-       console.log("Disconccee");
-   })
-    socket.on('rec_message',function (data) {
 
-        io.emit('get',data);//if we write io.broadcast.emit then teh message is recived by the user but we can not see the message we passsed
+    socket.on('rec_message',function (data) {
+        a.push(data);
+        io.emit('get',a);//if we write io.broadcast.emit then teh message is recived by the user but we can not see the message we passsed
         console.log(data);
     })
 
     // now every lister will be implimented on teh arugemt of the call back
 });
+*/
 
+io.on('connection',function(socket){
+    console.log(socket.id);
+
+    socket.on('rec_message',function(data){
+        a.push(data);
+        //console.log("inside socket on");
+        io.emit('get', data);
+        //console.log("after emit");
+    });
+
+    socket.emit('all', a);
+});
 server.listen(5000,function () {
     console.log("My http server is running ");
-})
+});
+
 
 /*we are using socetss, which require the http server,, but we want to make the server using the express
 * now what happpens is when we write http.createserver()  this cerates a empty server but when i pass appp as argument
